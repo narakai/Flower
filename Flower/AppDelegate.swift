22829b9da03ee4155848980ea606078ff76c33c8
laileon <leon@igola.com>
2017-12-26 11:08:58 +0800

    full code

--- Flower/AppDelegate.swift
+++ Flower/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let rootVC = ViewController()
+        // 创建窗口
+        window = UIWindow(frame: LLRect)
+        // 设置窗口的根控制器
+        window?.rootViewController = rootVC
+        // 显示窗口
+        window?.makeKeyAndVisible()
         return true
     }
 
